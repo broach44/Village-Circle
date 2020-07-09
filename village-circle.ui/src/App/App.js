66fd27185d47bd6c8403133f5d8baf18abc83fc7
Crystal Broach <crysland@gmail.com>
2020-07-08 19:10:47 -0500

    front end setup complete

---
+++ village-circle.ui/src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Village Circle</h1>
+      <Button color='teal' className='coolButton'>Coolest Button Ever!</Button>
+    </div>
+  );
+}
+
+export default App;
