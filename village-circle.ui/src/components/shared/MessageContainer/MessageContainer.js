2e170abdf3b0c0f4b015d8619cbfae378562ca47
Crystal Broach <crysland@gmail.com>
2020-07-11 15:56:13 -0500

    post message clearing text area input

--- village-circle.ui/src/components/shared/MessageContainer/MessageContainer.js
+++ village-circle.ui/src/components/shared/MessageContainer/MessageContainer.js
@@ -10,19 +10,39 @@ import PropTypes from 'prop-types';
 import './MessageContainer.scss';
 
 class MessageContainer extends React.Component {
+  state = {
+    currentPostMessage: '',
+    currentUser: null,
+  }
+
   static props = {
     messages: PropTypes.Array,
   }
 
+  resetInputBox = () => {
+    this.setState({ currentPostMessage: '' });
+  }
+
+  setPostMessage = (e) => {
+    this.setState({ currentPostMessage: e.target.value });
+  }
+
+  postEvent = (e) => {
+    e.preventDefault();
+    this.resetInputBox();
+    // TODO: Add backend function to post message then reset input box.
+  }
+
   render() {
     const { messages } = this.props;
+    const { currentPostMessage } = this.state;
     return (
       <div>
-      <Form>
+      <Form onSubmit={this.postEvent}>
         <Grid>
           <Grid.Row centered>
             <Grid.Column width={8}>
-              <Form.TextArea placeholder='Write a message...' />
+              <Form.TextArea placeholder='Write a message...' value={currentPostMessage} onChange={this.setPostMessage}/>
             </Grid.Column>
               <Button size='small'>Post Message</Button>
           </Grid.Row>
@@ -35,7 +55,12 @@ class MessageContainer extends React.Component {
             (messages.length === 0) ? <h3>Currently no one has posted to this board!</h3>
               : <Card.Group>
                 { messages.map((message) => <Card key={message.messageId} fluid raised className="singleMessage">
-                  <Card.Content description={message.messageText} /></Card>)}
+                  <Card.Content description={message.messageText} />
+                  <Button.Group basic size='small'>
+                    <Button icon='edit outline' />
+                    <Button icon='delete' />
+                  </Button.Group>
+                  </Card>)}
                 </Card.Group>
           }
         </Grid.Column>
