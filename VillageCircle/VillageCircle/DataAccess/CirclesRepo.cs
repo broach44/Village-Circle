ad46c02741338c23c9a83216c450efe94b1c3a37
Crystal Broach <crysland@gmail.com>
2020-07-10 22:04:12 -0500

    get all circles data point complete

---
+++ VillageCircle/VillageCircle/DataAccess/CirclesRepo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using VillageCircle.Models;
+using Dapper;
+
+namespace VillageCircle.DataAccess
+{
+    public class CirclesRepo
+    {
+        string connectionString;
+        public CirclesRepo(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("VillageCircle");
+        }
+        public IEnumerable<Circle> GetCircles()
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                return db.Query<Circle>("select * from Circle;");
+            }
+        }
+    }
+
+}
