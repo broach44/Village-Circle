958c9979a4fc40a18d149dca383ab467f75ac47e
Crystal Broach <crysland@gmail.com>
2020-07-27 20:51:15 -0500

    updated circle data adding boards to circles

--- VillageCircle/Sql Queries/CreateMessageBoardAndData.sql
+++ VillageCircle/Sql Queries/CreateMessageBoardAndData.sql
@@ -24,3 +24,23 @@ insert MessageBoard
 (boardName, boardDescription)
 values
 ('Math', 'Board for everyone to talk about their favorite topic...Math.')
+
+insert MessageBoard
+(boardName, boardDescription)
+values
+('American History', 'In order to not make the same mistakes we must study history.  This course is dedicated to strictly American History')
+
+insert MessageBoard
+(boardName, boardDescription)
+values
+('US Geography', 'Need a better sense of direction?  This circle may be for you.  Brush up on your United States Geography right here from the comfort of your own home')
+
+insert MessageBoard
+(boardName, boardDescription)
+values
+('Literature', 'This circle will be reviewing several literary works of art throughout the ages.')
+
+insert MessageBoard
+(boardName, boardDescription)
+values
+('Science', 'This circle will cover a broad spectrum of science topics.  From rocks and minerals to animals and plants, we can learn together about the world around us.')
