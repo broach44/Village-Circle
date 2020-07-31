c219728315e3b745f6de88705a6257af2bd3503a
Crystal Broach <crysland@gmail.com>
2020-07-30 19:12:15 -0500

    get all announcements by circleId complete

---
+++ VillageCircle/VillageCircle/DataAccess/AnnouncementsRepo.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using VillageCircle.Models;
+
+namespace VillageCircle.DataAccess
+{
+    public class AnnouncementsRepo
+    {
+        string connectionString;
+        public AnnouncementsRepo(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("VillageCircle");
+        }
+
+        public IEnumerable<Announcement> GetAllAnnouncements(int circleId)
+        {
+            var sql = @"select * from [Announcements]
+                        where CircleId = @CircleId;";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { CircleId = circleId };
+                var result = db.Query<Announcement>(sql, parameters);
+                return result;
+            }
+        }
+
+        public Announcement AddAnnouncement(Announcement announcementToAdd)
+        {
+            return null;
+        }
+    }
+}
