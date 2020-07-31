837897a957777543560c3374529d3503d716f99c
Crystal Broach <crysland@gmail.com>
2020-07-31 14:41:47 -0500

    create and delete announcement requests complete

--- VillageCircle/VillageCircle/DataAccess/AnnouncementsRepo.cs
+++ VillageCircle/VillageCircle/DataAccess/AnnouncementsRepo.cs
@@ -33,7 +33,38 @@ namespace VillageCircle.DataAccess
 
         public Announcement AddAnnouncement(Announcement announcementToAdd)
         {
-            return null;
+            DateTime dateTime = DateTime.Now;
+
+            var sql = @"
+                        insert into[Announcements](CircleId, AnnouncementDateTime, AnnouncementText)
+                        output inserted.*
+                        values(@CircleId, @AnnouncementDateTime, @AnnouncementText);
+                        ";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new
+                {
+                    CircleId = announcementToAdd.CircleId,
+                    AnnouncementDateTime = dateTime,
+                    AnnouncementText = announcementToAdd.AnnouncementText
+                };
+                var result = db.QueryFirstOrDefault<Announcement>(sql, parameters);
+                return result;
+            }
+        }
+
+        public Announcement DeleteAnnouncement(int announcementId)
+        {
+            var sql = @"DELETE from [Announcements]
+                        WHERE AnnouncementId = @AnnouncementId";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { AnnouncementId = announcementId };
+                var result = db.QueryFirstOrDefault<Announcement>(sql, parameters);
+                return result;
+            }
         }
     }
 }
