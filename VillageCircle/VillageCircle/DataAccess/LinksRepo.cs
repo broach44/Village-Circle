67cabf38a50154f91bd8ad45f200c292d147ef1c
Crystal Broach <crysland@gmail.com>
2020-07-31 14:58:15 -0500

    post and delete link requests complete

--- VillageCircle/VillageCircle/DataAccess/LinksRepo.cs
+++ VillageCircle/VillageCircle/DataAccess/LinksRepo.cs
@@ -29,5 +29,41 @@ namespace VillageCircle.DataAccess
                 return result;
             }
         }
+
+        public Link AddLink(Link linkToAdd)
+        {
+            var sql = @"
+                        insert into[Links](LinkTitle, LinkDescription, LinkUrl, IsAvailable, CircleId)
+                        output inserted.*
+                        values(@LinkTitle, @LinkDescription, @LinkUrl, 1, @CircleId);
+                        ";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new
+                {
+                    LinkTitle = linkToAdd.LinkTitle,
+                    LinkDescription = linkToAdd.LinkDescription,
+                    LinkUrl = linkToAdd.LinkUrl,
+                    CircleId = linkToAdd.CircleId
+                };
+                var result = db.QueryFirstOrDefault<Link>(sql, parameters);
+                return result;
+            }
+        }
+
+        public Link DeleteLink(int linkId)
+        {
+            var sql = @"update[Links]
+                        set IsAvailable = 0
+                        where LinkId = @LinkId;";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { LinkId = linkId };
+                var result = db.QueryFirstOrDefault<Link>(sql, parameters);
+                return result;
+            }
+        }
     }
 }
