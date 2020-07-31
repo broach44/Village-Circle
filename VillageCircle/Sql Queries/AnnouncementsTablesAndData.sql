4f63c7ad850a9f5d620bb705e2ccaa7244563f89
Crystal Broach <crysland@gmail.com>
2020-07-31 10:37:19 -0500

    get all links complete

---
+++ VillageCircle/Sql Queries/AnnouncementsTablesAndData.sql
@@ -0,0 +1,20 @@
+create table [Announcements]
+(
+	announcementId int identity(1,1) not null,
+	circleId int not null,
+	announcementDateTime datetime not null,
+	announcementText varchar(max) not null
+);
+
+insert into [Announcements]
+(circleId, announcementDateTime, announcementText)
+values
+(1, '2020-07-29', 'This is my first announcement and I am super excited to welcome you all to this Circle!')
+
+insert into [Announcements]
+(circleId, announcementDateTime, announcementText)
+values
+(2, '2020-07-28', 'This is another test exmple of the best announcement that you have ever read.')
+
+select * from [Announcements]
+where CircleId = 1;
---
+++ VillageCircle/VillageCircle/Controllers/LinksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VillageCircle.DataAccess;
+
+namespace VillageCircle.Controllers
+{
+    [Route("api/links")]
+    [ApiController]
+    public class LinksController : ControllerBase
+    {
+        LinksRepo _linksRepository;
+        public LinksController(LinksRepo linkRepository)
+        {
+            _linksRepository = linkRepository;
+        }
+
+        // api/links/{circleId}
+        [HttpGet("{circleId}")]
+        public IActionResult GetAllLinksByCircleId(int circleId)
+        {
+            var links = _linksRepository.GetAllLinks(circleId);
+            return Ok(links);
+        }
+
+    }
+}
