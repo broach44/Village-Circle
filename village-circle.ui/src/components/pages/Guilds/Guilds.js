ae02377f60e9cee548e2c08efef1528f68f794c8
Crystal Broach <crysland@gmail.com>
2020-08-01 12:01:33 -0500

    main guild page complete ui

--- village-circle.ui/src/components/pages/Guilds/Guilds.js
+++ village-circle.ui/src/components/pages/Guilds/Guilds.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Card } from 'semantic-ui-react';
+import GuildCard from '../../shared/GuildCard/GuildCard';
+
+import guildsData from '../../../helpers/guildsData';
 
 import './Guilds.scss';
 
 class Guilds extends React.Component {
+  state = {
+    guilds: [],
+  }
+
+  static props = {
+    authed: PropTypes.bool,
+  }
+
+  getGuildsData = () => {
+    guildsData.getAllGuilds()
+      .then((result) => this.setState({ guilds: result }))
+      .catch((err) => console.error('err from getting all guilds', err));
+  }
+
+  componentDidMount() {
+    this.getGuildsData();
+  }
+
   render() {
     return (
       <div className="Guilds">
-        <h2>Guilds</h2>
+        <Card.Group itemsPerRow={4} centered stackable>
+          {
+            this.state.guilds.map((guild) => <GuildCard key={guild.guidId} guild={guild} authed={this.props.authed} />)
+          }
+        </Card.Group>
       </div>
     );
   }
---
+++ village-circle.ui/src/helpers/guildsData.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import apiKeys from './apiKeys.json';
+
+const baseUrl = apiKeys.databaseURL;
+
+const getAllGuilds = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/guilds`)
+    .then((result) => {
+      const guilds = result.data;
+      resolve(guilds);
+    })
+    .catch((err) => reject(err));
+});
+
+const getGuildById = (guildId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/guilds/${guildId}`)
+    .then((result) => {
+      const guild = result.data;
+      resolve(guild);
+    })
+    .catch((err) => reject(err));
+});
+
+const verifyMembership = (userId, guildId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/guilds/memberverify/${userId}/${guildId}`)
+    .then((result) => {
+      const isMember = result.data;
+      resolve(isMember);
+    })
+    .catch((error) => reject(error));
+});
+
+const getGuildsByUser = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/guilds/owner/${userId}`)
+    .then((result) => {
+      const guilds = result.data;
+      resolve(guilds);
+    })
+    .catch((error) => reject(error));
+});
+
+const joinGuild = (memberInfo) => axios.post(`${baseUrl}/guilds/newMember`, memberInfo);
+
+const createNewCircle = (guildObj) => axios.post(`${baseUrl}/guilds`, guildObj);
+
+export default {
+  getAllGuilds,
+  getGuildById,
+  verifyMembership,
+  joinGuild,
+  getGuildsByUser,
+  createNewCircle,
+}
